ffacbb48d948cd7b02abb7c67e31cf37e0a01b4c
Aditya Mishra <adityamishra2217@gmail.com>
2023-04-15 16:19:30 +0530

    Updated thread1

--- threads/thread1/app/src/main/java/com/aditya/thread1/MainActivity.kt
+++ threads/thread1/app/src/main/java/com/aditya/thread1/MainActivity.kt
@@ -1,29 +1,79 @@
 package com.aditya.thread1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.aditya.thread1.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    lateinit var thread1: NewThread
+    private lateinit var binding: ActivityMainBinding
+    //Creating handler to pass messages and runnables between threads.
+    private val mHandler: Handler = Handler()
+    private var isStopped: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.btnstart.setOnClickListener {
-            val seconds = binding.etSeconds.text.toString().toInt()
-            thread1 = NewThread(seconds)
-            thread1.start()
+            isStopped = false
+            val secondsString = binding.etSeconds.text.toString()
+            val seconds = secondsString.toIntOrNull() ?: 10
+            val runnable = NewRunnable(seconds)
+            Thread(runnable).start()
         }
-     binding.btnstop.setOnClickListener {
-         
-     }
+
+        binding.btnstop.setOnClickListener {
+            isStopped = true
+        }
     }
 
+    inner class NewRunnable(val seconds: Int) : Runnable {
+        override fun run() {
+            Log.i(TAG, "Running in thread ${Thread.currentThread().name}")
+            for (i in 1..seconds) {
+                if (isStopped) return
+                if (i == 5) {
+                    //This Won't work as UI components can be updated on UI thread only
+                    //*  binding.btnstart.text = "50%" */
+                    //First approach -> Using mHandler associated with UI thread
+                   /** mHandler.post(object : Runnable{
+                        override fun run() {
+                            binding.btnstart.text = "50%"
+                        }
+                    })*/
 
+                    //Second approach -> Using Looper associated with mainHandler and
+                     // using it with Handler initialised in BACKGROUND thread
+                    /*val threadHandler = Handler(Looper.getMainLooper())
+                    threadHandler.post(object : Runnable{
+                        override fun run() {
+                            binding.btnstart.text = "50%"
+                        }
+
+                    })*/
+
+
+                    //Third Approach -> Using a Predefined function
+                    //runOnUiThread
+
+                    runOnUiThread(object : Runnable{
+                        override fun run() {
+                            binding.btnstart.text = "50%"
+                        }
+                    })
+
+                }
+
+                    Thread.sleep(1000)
+                    Log.i(TAG, "Thread time $i")
+            }
+        }
+    }
 }
