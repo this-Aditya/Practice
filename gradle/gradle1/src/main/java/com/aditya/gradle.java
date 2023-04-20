2daf57d4d3a132b458fbed0b62accff74f4d444f
Aditya Mishra <adityamishra2217@gmail.com>
2023-04-21 00:43:27 +0530

    gradle added

---
+++ gradle/gradle1/src/main/java/com/aditya/gradle.java
@@ -0,0 +1,7 @@
+package com.aditya;
+
+public class gradle {
+    public static void main(String[] args) {
+        System.out.println("Hello gradle ");
+    }
+}
---
+++ gradle/gradle1/src/test/java/com/aditya/GradleTest.java
@@ -0,0 +1,10 @@
+package com.aditya;
+
+import org.junit.Test;
+
+public class GradleTest {
+    @Test
+    public void verifyRunning() {
+        gradle.main(new String[]{});
+    }
+}
