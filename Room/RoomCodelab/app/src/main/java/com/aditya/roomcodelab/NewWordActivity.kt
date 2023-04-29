f4e0f7a87eafed4205012743e6e53891b4af5db9
Aditya Mishra <adityamishra2217@gmail.com>
2023-04-30 01:38:05 +0530

    added room project completely

---
+++ Room/RoomCodelab/app/src/main/java/com/aditya/roomcodelab/NewWordActivity.kt
@@ -0,0 +1,11 @@
+package com.aditya.roomcodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
