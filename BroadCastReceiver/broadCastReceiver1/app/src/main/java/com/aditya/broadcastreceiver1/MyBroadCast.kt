eb6674eaca08ce1bc09a5973d195ea523137a726
Aditya Mishra <adityamishra2217@gmail.com>
2023-05-15 21:27:33 +0530

    added memoryLeaks and Broadcast receiver examples

---
+++ BroadCastReceiver/broadCastReceiver1/app/src/main/java/com/aditya/broadcastreceiver1/MyBroadCast.kt
@@ -0,0 +1,11 @@
+package com.aditya.broadcastreceiver1
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyBroadCast: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action =  )
+    }
+}
